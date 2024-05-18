68edac32be3dc0840ba05b4808797930ae9bc696
Aman Kumar <9k725304@gmail.com>
2024-05-18 13:58:55 +0530

    add database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user"(name, mobile, pan, managerId) VALUES('Aman Kumar', '54654654', 'sd54sd5s', 1);
+INSERT INTO "user"(name, mobile, pan, managerId) VALUES('Devraj Saini', '54654654', 'sd54sd5s', 2);
+INSERT INTO "user"(name, mobile, pan, managerId) VALUES('Vikash Saini', '54654654', 'sd54sd5s', 3);
